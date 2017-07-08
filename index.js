debe3c3f0cc2a0ef096afcfb1a6ab7632f261bc2
tyleragreen <tgreen8091@gmail.com>
2017-07-08 21:06:54 +0000

    Export more symbols for use in gtfs-graph

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const TransitGraph = require('./lib/graph/graph.js');
+const TransitGraph = require('./lib/graph.js');
+const Stop = require('./lib/stop.js');
+const Edge = require('./lib/edge.js');
+const EdgeList = require('./lib/edgeList.js');
+const Route = require('./lib/route.js');
+const traversals = require('./lib/traversals.js');
 
-module.exports = TransitGraph;
+module.exports = {
+  TransitGraph,
+  Stop,
+  Edge,
+  EdgeList,
+  Route,
+  traversals
+};
